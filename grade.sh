0b347479d1c14597d25c3b78383a9ff78740f5ab
shjucsd <shj009@ucsd.edu>
2023-05-10 19:14:11 -0700

    lab work

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -14,3 +15,42 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+contains= $false
+for file in `find student-submission`
+do
+  if [[ -f $file ]] && [[ $file == ListExamples.java ]]
+  then
+    $contains= $true
+  fi
+done
+
+if [[ $contains == $true ]]
+then
+  cp student-submission/ListExamples.java grading-area/
+else
+  echo "File not found."
+  exit 1
+fi
+
+cp TestListExamples.java grading-area
+
+set +e
+
+javac grading-area/ListExamples.java >grading-area/errors.txt 2>&1
+if [[ $? -ne 0 ]]
+then
+  echo "Student File not compiled."
+  exit 2
+fi
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/TestListExamples.java > grading-area/errors.txt 2>&1
+if [[ $? -ne 0 ]]
+then
+  echo "Grading File not compiled."
+  exit 2
+fi
+
+
+
